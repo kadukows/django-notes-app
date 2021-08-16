b5e9076edfc395af185fc834d16b6e48598cc3ca
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-08-17 00:47:36 +0200

    models done

--- api/models.py
+++ api/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
