d60be8207004062405c4079c8a2a95568c918357
Maksymilian Kadukowski <maksymilian.kadukowski@gmail.com>
2021-08-18 00:28:22 +0200

    hello typescript + react + stuff

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const App = () => {
+    return <h1>Hello react</h1>;
+};
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "base.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'ts-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".json", ".ts", ".tsx"],
+  },
+};
